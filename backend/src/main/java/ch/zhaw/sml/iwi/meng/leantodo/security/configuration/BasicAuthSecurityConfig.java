9aaf9d9971a59c33e7eb71463edc4acbd7bd374e
peterheinrich <peter.heinrich@its-heinrich.ch>
2022-07-04 16:00:58 +0200

    Upgrading to newest springboot version

--- backend/src/main/java/ch/zhaw/sml/iwi/meng/leantodo/security/configuration/BasicAuthSecurityConfig.java
+++ backend/src/main/java/ch/zhaw/sml/iwi/meng/leantodo/security/configuration/BasicAuthSecurityConfig.java
@@ -1,59 +1,60 @@
 package ch.zhaw.sml.iwi.meng.leantodo.security.configuration;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
-@Order(2)
-public class BasicAuthSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
+public class BasicAuthSecurityConfig {
     
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
     
-    @Autowired
-    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
-    auth.jdbcAuthentication().dataSource(jdbcTemplate.getDataSource())
-        .usersByUsernameQuery(
-            "SELECT login_name, password_hash, true FROM user WHERE login_name=?")
-        .authoritiesByUsernameQuery(
-            "SELECT user_login_name, roles_role_name FROM user_roles WHERE user_login_name=?")
-        .passwordEncoder(passwordEncoder());       
-    }   
-    
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-            http.antMatcher("/auth/token")
+    @Bean
+    public UserDetailsService jdbcUserDetailsService(DataSource dataSource) {
+        String userQuery = "SELECT login_name, password_hash, true FROM usertable WHERE login_name=?";
+        String roleQuery = "SELECT user_login_name, roles_role_name FROM usertable_roles WHERE user_login_name=?";
+            
+        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+
+        users.setUsersByUsernameQuery(userQuery);
+        users.setAuthoritiesByUsernameQuery(roleQuery);
+
+        return users;
+    }
+
+    @Bean
+    @Order(2)
+    public SecurityFilterChain filterChainAuthBasic(HttpSecurity http) throws Exception {
+        http.antMatcher("/auth/token")
                     .httpBasic().and()
                     .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                     .authorizeRequests()
-                        .antMatchers(HttpMethod.OPTIONS,"/auth/token").permitAll()
-                    .anyRequest().authenticated()
-            ;     
-    }   
+                    .antMatchers(HttpMethod.OPTIONS,"/auth/token").permitAll()
+                    .anyRequest().authenticated();
+        return http.build();
+    }
     
     @Bean
-    @Override
-    public JdbcUserDetailsManager userDetailsService() {
-        JdbcUserDetailsManager manager = new JdbcUserDetailsManager();
-        manager.setJdbcTemplate(jdbcTemplate);
-        return manager;
-    }    
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 
 }
--- backend/src/main/java/ch/zhaw/sml/iwi/meng/leantodo/security/configuration/TokenCookieAuthSecurityConfigAuth.java
+++ backend/src/main/java/ch/zhaw/sml/iwi/meng/leantodo/security/configuration/TokenCookieAuthSecurityConfigAuth.java
@@ -1,34 +1,37 @@
 package ch.zhaw.sml.iwi.meng.leantodo.security.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import ch.zhaw.sml.iwi.meng.leantodo.security.TokenAuthenticationFilter;
 
 
 @Configuration
-@Order(3)
-public class TokenCookieAuthSecurityConfigAuth extends WebSecurityConfigurerAdapter {
+public class TokenCookieAuthSecurityConfigAuth {
 
    
     @Value("${jwt.secret}")
     private String secret;
     
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    @Order(3)
+    public SecurityFilterChain filterChainAuthJWT(HttpSecurity http) throws Exception {
         http.antMatcher("/auth/**")
                 .addFilterBefore(new TokenAuthenticationFilter(secret), UsernamePasswordAuthenticationFilter.class)
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .httpBasic().disable()
                 .authorizeRequests()
-                    .antMatchers(HttpMethod.OPTIONS,"/auth/**").permitAll()
-                .anyRequest().authenticated();        
+                .antMatchers(HttpMethod.OPTIONS,"/auth/**").permitAll()
+                .anyRequest().authenticated();    
+        return http.build();    
     }   
     
    
