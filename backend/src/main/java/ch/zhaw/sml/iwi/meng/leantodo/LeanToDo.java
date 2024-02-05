59048ab09227ff7dde633d613411e6d3a44409ce
peterheinrich <peter.heinrich@its-heinrich.ch>
2024-02-05 09:16:04 +0100

    Removing unused dependencies

--- backend/src/main/java/ch/zhaw/sml/iwi/meng/leantodo/LeanToDo.java
+++ backend/src/main/java/ch/zhaw/sml/iwi/meng/leantodo/LeanToDo.java
@@ -4,12 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import ch.zhaw.sml.iwi.meng.leantodo.entity.Role;
 import ch.zhaw.sml.iwi.meng.leantodo.entity.RoleRepository;
