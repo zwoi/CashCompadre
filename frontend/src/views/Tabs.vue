f95e239f5ef1a2197bc8dc5dce833fd087a17598
AleCecc <103599554+AleCecc@users.noreply.github.com>
2024-02-06 11:23:56 +0100

    router

--- frontend/src/views/Tabs.vue
+++ frontend/src/views/Tabs.vue
@@ -3,17 +3,17 @@
     <ion-tabs>
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
-        <ion-tab-button tab="todo" href="/tabs/todo">
+        <ion-tab-button tab="todo" href="/tabs/Dashboard">
           <ion-icon :icon="homeSharp" />
           <ion-label>Dashboard</ion-label>
         </ion-tab-button>
           
-        <ion-tab-button tab="projects" href="/tabs/projects">
+        <ion-tab-button tab="projects" href="/tabs/Categories">
           <ion-icon :icon="fileTrayFullSharp" />
           <ion-label>Category</ion-label>
         </ion-tab-button>
         
-        <ion-tab-button tab="timerecord" href="/tabs/timerecord">
+        <ion-tab-button tab="timerecord" href="/tabs/User">
           <ion-icon :icon="personCircleSharp" />
           <ion-label>Profile</ion-label>
         </ion-tab-button>
