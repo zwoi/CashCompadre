0194e03d67958dcd5b40dbc1de74bbf8343a71f4
zwoi <romeo.meyer@hotmail.com>
2024-02-06 16:08:15 +0100

    null

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,10 +1,6 @@
 INSERT INTO TO_DO (title,archived,done,owner) VALUES ('Finish this app',false,false, 'user' );
 INSERT INTO TO_DO (title,archived,done,owner) VALUES ('Another task',false,false, 'user' );
 
-<<<<<<< HEAD
 INSERT INTO CATEGORY (LIMIT_AMOUNT, NAME, ID) Values (333,'Essen',0);
-=======
---INSERT INTO CATEGORY (LIMIT_AMOUNT, NAME) Values (333,'Essen');
->>>>>>> 5a615b1213d155953c9baac282379d0d2b6582ce
 --INSERT INTO EXPENSES (id, amount, note, done) VALUES ('0', '13', 'Lunch at the new place', 1994-12-15);
 --INSERT INTO USERTABLE (login_Name, password_Hash) VALUES ('user1', "$2a$10$o8hbITIWZFF7Sm6PKSaddO/whcqnzcl5b7ekk1Yu7WKmU33EKsVk.")
