23f8214573556a1d11c79c0175f7328873d1bdf8
Nambudy <sonnam.nguyen99@gmail.com>
2024-02-06 13:36:02 +0100

    controller

--- backend/src/main/java/ch/zhaw/sml/iwi/meng/leantodo/controller/CategoryController.java
+++ backend/src/main/java/ch/zhaw/sml/iwi/meng/leantodo/controller/CategoryController.java
@@ -1,6 +1,7 @@
 package ch.zhaw.sml.iwi.meng.leantodo.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -17,5 +18,15 @@ public class CategoryController {
     public List<Category> listAllCategories(String loginName) {
         return categoryRepository.findByName(loginName);
     }
+
+    public void persistCategory(Category newProject, String name) {
+        newProject.setId(null);
+        // We only create empty categories
+        newProject.getExpenses().clear();
+        newProject.setName(name);
+        categoryRepository.save(newProject);
+    }
+
+    
 
 }
