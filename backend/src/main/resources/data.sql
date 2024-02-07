cfce8c24143302af3f72f64e0b81841e3f10e11c
Nambudy <sonnam.nguyen99@gmail.com>
2024-02-07 10:59:32 +0100

    create category

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO TO_DO (title,archived,done,owner) VALUES ('Finish this app',false,false, 'user' );
-INSERT INTO TO_DO (title,archived,done,owner) VALUES ('Another task',false,false, 'user' );
+--INSERT INTO TO_DO (title,archived,done,owner) VALUES ('Finish this app',false,false, 'user' );
+--INSERT INTO TO_DO (title,archived,done,owner) VALUES ('Another task',false,false, 'user' );
 
 INSERT INTO CATEGORY (LIMIT_AMOUNT, NAME, ID) Values (333,'Essen',0);
 --INSERT INTO EXPENSES (id, amount, note, done) VALUES ('0', '13', 'Lunch at the new place', 1994-12-15);
